d7e80eb54dcaef1fc3f003d2c576e35c37d6a3bf
Dipika Bari <dipikab@y7mail.com>
2022-05-10 16:26:50 +0100

    Added files for Network layer

---
+++ PixabayChallenge/Networking/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  PixabayChallenge
+//
+//  Created by Dipika Bari on 10/05/2022.
+//
+
+import Foundation
+
+struct API {
+    static let key  = "13197033-03eec42c293d2323112b4cca6"
+}
